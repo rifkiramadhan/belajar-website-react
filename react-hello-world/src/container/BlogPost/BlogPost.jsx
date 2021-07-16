31ef32545db2ff3c9f0de81fe08314e2471765f6
Rifki Ramadhan <riifkiramadhan2@gmail.com>
2021-07-17 00:32:13 +0700

    Mengirim file latihan 11-15 Fix CRUD React JS

--- react-hello-world/src/container/BlogPost/BlogPost.jsx
+++ react-hello-world/src/container/BlogPost/BlogPost.jsx
@@ -7,22 +7,19 @@ class BlogPost extends Component {
 
     // Siapkan state
     state = {
-        post: []
+        post: [],
+        formBlogPost: {
+            id: 1,
+            title: '',
+            body: '',
+            userId: 1
+        },
+        isUpdate: false
     }
 
-    // Ketika component berhasil dimounting
-    componentDidMount() {
-        // 1. Menggunakan Fetch
-        // fetch('https://jsonplaceholder.typicode.com/posts')
-        // .then(response => response.json())
-        // .then(json => {
-        //     this.setState({
-        //         post: json
-        //     })
-        // })
-
+    getPostApi = () => {
         // 2. Menggunakan Axios
-        axios.get('https://jsonplaceholder.typicode.com/posts')
+        axios.get('http://localhost:3004/posts?_sort=id&_order=desc')
         .then((result) => {
             // console.log(result.data);
             this.setState({
@@ -31,14 +28,117 @@ class BlogPost extends Component {
         })
     }
 
+    postDataToApi = () => {
+        axios.post('http://localhost:3004/posts', this.state.formBlogPost).then((res) => {
+            console.log(res);
+            
+            // Memanggil data yang telah disubmit
+            this.getPostApi();
+
+            this.setState({
+                // isUpdate: false,
+                formBlogPost: {
+                    id: 1,
+                    title: '',
+                    body: '',
+                    userId: 1
+                },
+            })
+        }, (err) => {
+            console.log('error: ', err);
+        })
+    }
+
+    PutDataToApi = () => {
+        axios.put(`http://localhost:3004/posts/${this.state.formBlogPost.id}`, this.state.formBlogPost).then(res => {
+            console.log(res);
+            this.getPostApi();
+            this.setState({
+                isUpdate: false,
+                formBlogPost: {
+                    id: 1,
+                    title: '',
+                    body: '',
+                    userId: 1
+                },
+            })
+        })
+    }
+
+    handleRemove = (data) => {
+        // console.log(data);
+        axios.delete(`http://localhost:3004/posts/${data}`).then((res) => {
+            // console.log(res);
+            this.getPostApi()
+        })
+    }
+
+    handleUdate = (data) => {
+        console.log(data);
+        this.setState({
+            formBlogPost: data,
+            isUpdate: true
+        })
+        // axios.put(`http://localhost:3004/posts/${data}`, this.state.formBlogPost).then(res => {
+        //     console.log(res);
+        //     this.getPostApi();
+        // })
+
+    }
+
+    // Membuat sebuah handler/method ketika input/ketika form berubah
+    handleFormChange = (event) => {
+        // console.log('form change', event.target);
+        let formBlogPostNew = {...this.state.formBlogPost};
+        let timestamp = new Date().getTime();
+        // console.log(timestamp);
+        
+        if(!this.state.isUpdate) {
+            formBlogPostNew['id'] = timestamp;
+
+        }
+
+        // console.log(event.target.name);
+        formBlogPostNew[event.target.name] = event.target.value;
+        // console.log('init state: ', this.state.formBlogPost);
+        // console.log('new value: ', formBlogPostNew);
+        // let title = event.target.value
+        this.setState({
+            formBlogPost: formBlogPostNew
+        }) 
+    }
+
+    handleSubmit = () => {
+        if(this.state.isUpdate) {
+            this.PutDataToApi();
+        }else{
+            this.postDataToApi();
+        }
+        // console.log(this.state.formBlogPost);
+    }
+
+    // Ketika component berhasil dimounting
+    componentDidMount() {
+        // Remove fungsi code yang tidak digunakan
+        this.getPostApi();
+    }
+
     // Kita akan memanggil API dari post ini
     render() {
         return(
             <Fragment>
                 <p className="section-title">Blog Post</p>
+                
+                <div className="form-add-post">
+                    <label className="title">Title</label>
+                    <input type="text" value={this.state.formBlogPost.title} name="title" placeholder="add title" onChange={this.handleFormChange}/>
+                    <label htmlFor="body">Blog Content</label>
+                    <textarea name="body" id="body" cols="30" rows="10" value={this.state.formBlogPost.body} placeholder="add blog content" onChange={this.handleFormChange}></textarea>
+                    <button className="btn-submit" onClick={this.handleSubmit}>Simpan</button>
+                </div>
                 {
                     this.state.post.map(post => {
-                        return <Post key={post.id} title={post.title} desc={post.body} />
+                        return <Post key={post.id} data={post} remove={this.handleRemove} update={this.handleUdate}/>
 
                     })
                 }
--- react-hello-world/src/container/Post/Post.jsx
+++ react-hello-world/src/container/Post/Post.jsx
@@ -7,8 +7,10 @@ const Post = (props) => {
                 <img src="https://placeimg.com/200/150/tech" alt="dummy" />
            </div>
                 <div className="content">
-                <p className="title">{props.title}</p>
-                <p className="desc">{props.desc}</p>
+                <p className="title">{props.data.title}</p>
+                <p className="desc">{props.data.body}</p>
+                <button className="update" onClick={() => props.update(props.data)}>Update</button>
+                <button className="remove" onClick={() => props.remove(props.data.id)}>Remove</button>
             </div>
         </div>
     )
